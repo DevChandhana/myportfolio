eda93544a39756361958c4f659e6ea587265e383
Chandhana <chandhanareddy1997@gmail.com>
2022-07-19 23:18:52 +0530

    navbar

---
+++ src/components/banner/index.js
@@ -0,0 +1,4 @@
+const Banner = () => {
+  return <div className="h-screen bg-green-300">Im banner section</div>;
+};
+export default Banner;
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,8 +1,16 @@
+import { AiFillHome, AiFillFileText, AiFillMail } from "react-icons/ai";
+import { FaUserNinja, FaHeart } from "react-icons/fa";
+// import { MdWork } from "react-icons/md";
 import "./index.scss";
+
 const Navbar = () => {
   return (
     <div className="Container">
-      <p className="text">Im Navbar</p>
+      <AiFillHome size={"1.7em"} title="Home" />
+      <FaUserNinja size={"1.7em"} title="About" />
+      <AiFillFileText size={"1.7em"} title="Projects" />
+      <FaHeart size={"1.7em"} title="Interests" />
+      <AiFillMail size={"1.7em"} title="Contact" />
     </div>
   );
 };
