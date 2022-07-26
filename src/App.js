4927005c2781429999712173ecb4b3b20712a655
Chandhana <chandhanareddy1997@gmail.com>
2022-07-26 17:12:49 +0530

    redesigning

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import React from "react";
+import { Fragment } from "react";
 import "./App.css";
-//components
-import Navbar from "./components/navbar/index";
-import Banner from "./components/banner/index";
-import PageLoader from "./components/pageload";
-import About from "./components/about";
+// components
+import Home from "./components/home";
 function App() {
-  const [timer, setTimer] = React.useState(true);
-  React.useEffect(() => {
-    timeOut();
-    // eslint-disable-next-line
-  }, []);
-  const timeOut = () => {
-    setTimeout(() => {
-      setTimer(!timer);
-    }, 5000);
-  };
   return (
-    <div className="scrollbar-hide">
-      {timer ? (
-        <PageLoader />
-      ) : (
-        <>
-          <div className=" flex justify-center">
-            <Navbar />
-          </div>
-          <Banner />
-          <About />
-        </>
-      )}
-    </div>
+    <Fragment>
+      <Home />
+    </Fragment>
   );
 }
 
---
+++ src/components/home/index.js
@@ -0,0 +1,4 @@
+const Home = () => {
+  return <div>Im home screen i.e., banner</div>;
+};
+export default Home;
