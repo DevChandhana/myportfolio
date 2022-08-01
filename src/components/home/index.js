92a1699208882fc29b3610197651c5b00d661ffe
Chandhana <chandhanareddy1997@gmail.com>
2022-08-01 12:46:29 +0530

    about container

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,4 +1,13 @@
+import "./index.scss";
+import ProfilePic from "../../assets/image.jpeg";
 const Home = () => {
-  return <div>Im home screen i.e., banner</div>;
+  return (
+    <div className="home">
+      <div className="aboutBox">About container</div>
+      <div className="imageBox">
+        <img src={ProfilePic} alt="profile pic" className="img" />
+      </div>
+    </div>
+  );
 };
 export default Home;
