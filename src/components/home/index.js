79322aef18331f0e91cac54ec1e9e23cec84afb5
Chandhana <chandhanareddy1997@gmail.com>
2022-08-04 13:41:05 +0530

    cache

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,7 +3,14 @@ import ProfilePic from "../../assets/image.jpeg";
 const Home = () => {
   return (
     <div className="home">
-      <div className="aboutBox">About container</div>
+      <div className="aboutBox">
+        <h1 className="heading">I'm Chandhana Reddy</h1>
+        <h3 className="desc">
+          I'm a frontend developer who can build applications for both mobile
+          and web platforms. I have 2.8 years of experience as a Frontend
+          developer. Currently focusing on becoming a full-stack developer.
+        </h3>
+      </div>
       <div className="imageBox">
         <img src={ProfilePic} alt="profile pic" className="img" />
       </div>
